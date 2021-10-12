dce48dc36f7263804a8d876f8a1af378daad29f8
Joshua <joshuagordon2418@gmail.com>
2021-10-12 04:42:27 -0400

    Styling and functionality update

--- backend.js
+++ backend.js
@@ -1,29 +1,39 @@
-let intro = document.querySelector('.intro');
-let arrowSpan = document.querySelectorAll('.arrows');
-let name = document.querySelector('.nameAnimation');
+// Landing Screen Animation using GSAP 
+let timeline1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.navArea',
+        start: '0%',
+        end: '85%',
+        scrub: 1,
+    }
+});
+let timeline2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.navArea',
+        start: '0%',
+        end: '85%',
+        scrub: 1,
+    }
+});
+let timeline3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.navArea',
+        start: '0%',
+        end: '200%',
+        scrub: 1,
+        pin: true,
+        pinSpacing: false,
 
-window.addEventListener('DOMContentLoaded', ()=>{
-        setTimeout(()=>{
-
-            arrowSpan.forEach((span, idx)=>{
-                setTimeout(()=> {
-                    span.classList.add('active');
-                }, (idx + 1) * 400)
-            });
-
-            setTimeout(()=>{
-                arrowSpan.forEach((span, idx)=> {
-                    setTimeout(()=>{
-                        span.classList.remove('active');
-                        span.classList.add('fade');
-
-                    }, (idx + 1) * 50)
-                })
-            }, 2000);
-
-            setTimeout(()=>{
-                intro.style.top = '-100vh';
-            }, 2300)
-        });
+    }
 });
 
+timeline1.fromTo('.motto', {y: 0}, {y: -400});
+timeline2.fromTo('.logo', {scale: 6}, {scale: 1, top: "2rem", left: "3rem", x: "50%", y: "50%"});
+
+// Clearing textbox on click
+$('input:text').click(
+    function(){
+        $(this).val('');
+    });
+
+// Calling API for search bar entered
