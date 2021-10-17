621e5aa43e67b16d7fe2d74342e9f5997ce7ad2c
Joshua <joshuagordon2418@gmail.com>
2021-10-16 20:12:16 -0400

    Added animation update to scrolling and base styling of intro page

--- backend.js
+++ backend.js
@@ -1,8 +1,10 @@
 // Landing Screen Animation using GSAP 
+    // Timeline 1  and Timeline 2 animation effect
 let timeline1 = gsap.timeline({
     scrollTrigger: {
         trigger: '.navArea',
         start: '0%',
+        // How much of screen must be scrolled before animation takes effect
         end: '85%',
         scrub: 1,
     }
@@ -15,6 +17,7 @@ let timeline2 = gsap.timeline({
         scrub: 1,
     }
 });
+// After animation is complete, end value tells how long to keep the navbar pinned on top
 let timeline3 = gsap.timeline({
     scrollTrigger: {
         trigger: '.navArea',
@@ -26,8 +29,9 @@ let timeline3 = gsap.timeline({
 
     }
 });
-
+// Motto area is moved out of screen view on scroll
 timeline1.fromTo('.motto', {y: 0}, {y: -400});
+// Logo is moved from middle of screen to motto position
 timeline2.fromTo('.logo', {scale: 6}, {scale: 1, top: "2rem", left: "3rem", x: "50%", y: "50%"});
 
 // Clearing textbox on click
